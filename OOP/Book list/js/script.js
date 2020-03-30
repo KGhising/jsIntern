f61ebaa6890de3e902ded5b6327037c2db29c012
Kishor Ghising <bhfty99@gmail.com>
2020-03-30 15:02:07 +0545

    OOP Book list: implement input validation, display error message

--- OOP/Book list/js/script.js
+++ OOP/Book list/js/script.js
@@ -2,7 +2,7 @@
 
 // book constructor
 function Book(title, author, isbn){
-    this.titl = title;
+    this.title = title;
     this.author = author;
     this.isbn = isbn;
 }
@@ -19,12 +19,31 @@ UI.prototype.addBookToList = function(book){
     const row = document.createElement('tr');
     // appending columns
     row.innerHTML = `
-         <td>${book.title}</td> 
+         <td>${book.title}</td>
          <td>${book.author}</td>
-         <td>${book.ispn}</td>
+         <td>${book.isbn}</td>
          <td><a href = "#" class = "delete">X</a></td> 
     `;
     list.appendChild(row);
+}
+
+// error message
+UI.prototype.showAlert = function(message, className){
+    // create div
+    const div = document.createElement('div');
+    div.className = `alert ${className}`; 
+    div.appendChild(document.createTextNode(message));
+
+    // get parent element
+    const section = document.querySelector('.section');
+    const container = document.querySelector('.container');
+    // inserting error element
+    container.insertBefore(div, section);
+
+    // hide after 3 sec
+    setTimeout(function(){
+        document.querySelector('.alert').remove();
+    }, 3000);
 }
  // clear inputs
  UI.prototype.clearInput = function(){
@@ -37,18 +56,27 @@ document.getElementById('bookForm').addEventListener('submit',function(e){
     // getting value from input
     const title = document.getElementById('bookTitle').value,
           author = document.getElementById('bookAuthor').value,
-          ispn = document.getElementById('bookISBN').value
+          isbn = document.getElementById('bookISBN').value
 
     // instantiate book
-    const book = new Book(title, author, ispn);
+    const book = new Book(title, author, isbn);
     
     // instantiate UI
     const ui = new UI();
 
-    // add book to list
-    ui.addBookToList(book);
-    // clear input
-    ui.clearInput();
+    // validate input
+    if(title === '' || author === '' || isbn === ''){
+        // Error alert
+        ui.showAlert('Your inputs are empty, Please check!', 'error');
+    }else{
+        // add book to list
+        ui.addBookToList(book);
+        // show sucess message
+        ui.showAlert('Book added Successful', 'success');
+        // clear input
+        ui.clearInput();    
+    }
+
     // prevent default
     e.preventDefault();
 });
