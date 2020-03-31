5651c747d6d81be0508ffe9de001447aec2ef987
Kishor Ghising <bhfty99@gmail.com>
2020-03-31 16:23:49 +0545

    Asynchronous JS: work on json array for, multiple employee to display on browser

--- AsynchronousJS,Ajax&FetchAPI/workingWithAjaxandJSON/app.js
+++ AsynchronousJS,Ajax&FetchAPI/workingWithAjaxandJSON/app.js
@@ -1,7 +1,9 @@
 document.getElementById('btn1').addEventListener('click', loadEmployee);
 
-// load customer function
-function loadEmployee(){
+document.getElementById('btn2').addEventListener('click', loadEmployees);
+
+// load employee function single 
+function loadEmployee(e){
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'employee.json', true);
@@ -17,6 +19,33 @@ function loadEmployee(){
                 </ul>
             `;
             document.getElementById('employee').innerHTML = output;
+        }
+    }
+    
+    xhr.send();
+}
+
+// load employees function multi employee
+function loadEmployees(e){
+    console.log("hey");
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'employees.json', true);
+
+    xhr.onload = function(){
+        if(this.status == 200){
+            const employees = JSON.parse(this.response);
+            let output = '';
+            employees.forEach(function(employee){
+                output += `
+                <ul>
+                    <li>ID: ${employee.id}</li>
+                    <li>Name: ${employee.name}</li>
+                    <li>Company: ${employee.company}</li>
+                </ul>
+            `;
+            });
+            document.getElementById('employees').innerHTML = output;
         }
     }
     
