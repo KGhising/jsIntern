059ef182a4b558b14eecf0dcc56a3331e7e8bc83
Kishor Ghising <bhfty99@gmail.com>
2020-04-01 15:57:34 +0545

    Asynchronous JS: Custom HTTP library, implement POST data to library

--- AsynchronousJS,Ajax&FetchAPI/custom HTTP library/app.js
+++ AsynchronousJS,Ajax&FetchAPI/custom HTTP library/app.js
@@ -1,7 +1,23 @@
 const http = new HTTP;
 
 // GET post
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
+//     if(err){
+//         console.log(err);
+//     }    
+//     else{
+//         console.log(posts);
+//     }
+// });
+
+// Create data
+const data = {
+    title: 'Custom post',
+    body: 'Custom body'
+};
+
+//create POST
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, posts){
     if(err){
         console.log(err);
     }    
@@ -9,4 +25,3 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts){
         console.log(posts);
     }
 });
-
