23c7a08116fc720fef9cd012bfdac453a048d8cb
Kishor Ghising <bhfty99@gmail.com>
2020-04-02 10:58:16 +0545

    Asynchronous JS: Fetch API, get text data from .json and display on browser

--- AsynchronousJS_Ajax_FetchAPI/Fetch_API/app.js
+++ AsynchronousJS_Ajax_FetchAPI/Fetch_API/app.js
@@ -1,8 +1,8 @@
 document.getElementById('btn1').addEventListener('click', getText);
-document.getElementById('btn2')
+document.getElementById('btn2').addEventListener('click', getJSON);
 document.getElementById('btn2')
 
-// get Text function
+// get local Text function
 function getText(){
     fetch('test.txt')
     .then(function(response){
@@ -10,5 +10,22 @@ function getText(){
     }) 
     .then(function(data){
         document.getElementById('output').innerHTML = data;
+    });
+}
+
+// get local JSON function
+function getJSON(){
+    fetch('post.json')
+    .then(function(response){
+        return response.json();  
+    }) 
+    .then(function(data){
+        let output = '';
+        data.forEach(function(post){
+            output +=`
+            <li>${post.title}</li>
+            `;
+        });
+        document.getElementById('output').innerHTML = output; 
     });
 }
