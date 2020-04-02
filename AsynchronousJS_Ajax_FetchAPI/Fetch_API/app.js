2dc564a6968f6373d9f7f0c895d2d1f4cacafa89
Kishor Ghising <bhfty99@gmail.com>
2020-04-02 11:09:47 +0545

    Asynchronous JS: Fetch API, get .json file from external github api

--- AsynchronousJS_Ajax_FetchAPI/Fetch_API/app.js
+++ AsynchronousJS_Ajax_FetchAPI/Fetch_API/app.js
@@ -1,6 +1,6 @@
 document.getElementById('btn1').addEventListener('click', getText);
 document.getElementById('btn2').addEventListener('click', getJSON);
-document.getElementById('btn2')
+document.getElementById('btn3').addEventListener('click', getApiData);
 
 // get local Text function
 function getText(){
@@ -24,6 +24,23 @@ function getJSON(){
         data.forEach(function(post){
             output +=`
             <li>${post.title}</li>
+            `;
+        });
+        document.getElementById('output').innerHTML = output; 
+    });
+}
+
+// get from external API
+function getApiData(){
+    fetch('https://api.github.com/users')
+    .then(function(response){
+        return response.json();  
+    }) 
+    .then(function(data){
+        let output = '';
+        data.forEach(function(user){
+            output +=`
+            <li>${user.login}</li>
             `;
         });
         document.getElementById('output').innerHTML = output; 
