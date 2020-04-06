770b329cfdc8aaa7d4a1928c3c6532f89a6948b2
Kishor Ghising <bhfty99@gmail.com>
2020-04-06 13:22:38 +0545

    Error handling: use of metacharacter brackets[], quantifier{}, parenthesis()

--- Error_handling/Regular_expression/metaCharacter_symbol/app.js
+++ Error_handling/Regular_expression/metaCharacter_symbol/app.js
@@ -12,9 +12,25 @@ re = /h*llo/i; // * match any character 0 or more times
 re = /gre?a?y/i; // ? optionalcharacter a and e
 re = /gre?a?y\?/i; // \ escape character character
 
+// Brackets [] character sets
+re = /gr[ae]y/i; // must be a or e
+re = /[GF]ray/; // must be G or F
+re = /[^GF]ray/; // Match except G or F
+re = /[A-Z]ray/; // Match any uppercase character between A-Z  
+re = /[a-zA-Z]ray/; // Match any character between a-z or A-Z
+re = /[0-9]ray/; // Match any digit between
+
+// Braces {} -Quantifier
+re = /hel{2}o/i; // must occur exactly {m} times
+re = /hel{2,4}o/i; // must occur exactly between {n,m} times
+re = /hel{2,}o/i; // must occur atleast between {m,} times
+
+// parethesis () -grouping
+re = /([0-9]x){3}/; // Grouping [0-9]x 
+
 
 // strring to match
-const str = 'grey?';
+const str = '3x3x';
 const result = re.exec(str);
 console.log(result);
 
